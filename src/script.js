406f20b84c68e824f15b3f66df2325bfc93722b6
evertonsavio <everluca@outlook.com>
2022-02-26 14:47:25 -0300

    :art: animating meshes

--- src/script.js
+++ src/script.js
@@ -63,6 +63,8 @@ mesh3.position.y = - objectsDistance * 2;
 
 scene.add(mesh1, mesh2, mesh3)
 
+const sectionMeshes = [mesh1, mesh2, mesh3]
+
 //Light
 const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
 directionalLight.position.set(1, 1, 0)
@@ -115,6 +117,11 @@ const clock = new THREE.Clock()
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+
+    //Animate Meshes
+    for (const mesh of sectionMeshes) {
+        mesh.rotation.x = elapsedTime * 0.1
+    }
 
     // Render
     renderer.render(scene, camera)
